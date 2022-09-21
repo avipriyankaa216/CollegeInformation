05abf1269ce47d9ac209bb33b3fd33c79fe22a17
avipriyankaa216 <112318152+avipriyankaa216@users.noreply.github.com>
2022-09-21 20:40:21 +0530

    Update Department.java

--- src/org/college/Department.java
+++ src/org/college/Department.java
@@ -3,7 +3,7 @@ package org.college;
 public class Department extends College{
 	public void deptname() {
 		// TODO Auto-generated method stub
-		System.out.println("department name is ECE");
+		System.out.println("department name is ECE meena");
 
 	}
 	
